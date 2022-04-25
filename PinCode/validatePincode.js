f6add657266b1de43f3eba04121a318f91e79566
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-04-25 12:05:25 +0530

    added to validate pin code 400088

--- PinCode/validatePincode.js
+++ PinCode/validatePincode.js
@@ -1,3 +1,7 @@
 //Pincode Validation 
 let pincodeCheck = RegExp('^[1-9]{1}[0-9]{5}$');
 let pincodeCheck1 = RegExp('^[1-9]{1}[0-9]{2}[\\s][0-9]{3}$');
+
+//UC1 Pincode
+let sampleCheck1 = pincodeCheck.test(400088);
+console.log(sampleCheck1);
